8b6feb7682afb85a7ce1e9eb02e05e47bd7f9d84
Hugo S <92046586+Hushgan@users.noreply.github.com>
2022-02-02 17:56:06 +0100

    Update Aimbot sans commentaire.py

--- Aimbot sans commentaire.py
+++ Aimbot sans commentaire.py
@@ -32,9 +32,9 @@ def coordonnees():
     (x2, y2) = pyautogui.position()
     x3 = x2 - w2
     y3 = (y2 + 8) - h2
+    pyautogui.moveTo(x2 - w2, y2 - h2)
     for i in range(len(faces)):
         (x1, y1, w1, h1) = faces[nombre]
-        pyautogui.moveTo(x2 - w2, y2 - h2)
         pyautogui.moveTo(x3 + x1 +w1/2, y3 + y1+h1/2, duration=0.2)
         cv2.rectangle(image, (x1, y1), (x1 + w1, y1 + h1), (255, 255, 0), 2)
         cv2.imshow(texte_image, image)
